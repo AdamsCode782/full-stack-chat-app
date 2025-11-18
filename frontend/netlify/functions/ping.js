fdcade31c1bf1cb18264ef38cb5bbff5e9d4a48e
AdamsCode782 <asteeledesign@gmail.com>
2025-11-18 07:24:51 -0500

    Delete frontend/netlify/functions directory
    Removed folder with files related to netlify deployment

--- frontend/netlify/functions/ping.js
+++
@@ -1,8 +0,0 @@
-export default async () => {
-  try {
-    await fetch("https://full-stack-chat-app-rm9v.onrender.com");
-    return new Response("Pinged backend", { status: 200 });
-  } catch (err) {
-    return new Response("Failed to ping backend", { status: 500 });
-  }
-};
