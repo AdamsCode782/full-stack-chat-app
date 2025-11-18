d420bc77f8609b44ae65a329086914c7847417f9
AdamsCode782 <asteeledesign@gmail.com>
2025-11-18 10:49:27 -0500

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,11 +1,17 @@
 import axios from "axios";
 
 const DEV = import.meta.env.MODE === "development";
-
-//RENDER
 const PROD_URL = "https://full-stack-chat-app-rm9v.onrender.com";
 
 export const axiosInstance = axios.create({
   baseURL: DEV ? "http://localhost:5001/api" : `${PROD_URL}/api`,
-  withCredentials: true,
+});
+
+// automatically attach JWT
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("jwt");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
